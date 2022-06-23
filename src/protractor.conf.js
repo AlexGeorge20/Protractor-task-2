64fda8506047045cfe55eef7ae6bcaec64cefaf7
AlexGeorge20 <alexander@neoito.com>
2022-06-23 17:12:45 +0530

    working-protactor

--- src/protractor.conf.js
+++ src/protractor.conf.js
@@ -1,11 +1,11 @@
 exports.config = {
   framework: 'jasmine', //Type of Framework used 
   directConnect:true, 
-  specs: ['spec.js'], //Name of the Specfile
+  specs: ['home-spec.ts'], //Name of the Specfile
   onPrepare() { 
      //global test set-up goes here 
      require('ts-node').register({ 
-      project: require('path').join(__dirname, './tsconfig.json') // Relative path of tsconfig.json file 
+      project: require('path').join(__dirname, '../tsconfig.json') // Relative path of tsconfig.json file 
   });
 },
   onComplete() { 
